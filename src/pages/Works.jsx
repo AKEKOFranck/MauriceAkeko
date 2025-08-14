7a80cb18db736d79469478b36791f6de1db1e330
AKEKOFranck <142504737+MauriceAkeko@users.noreply.github.com>
2025-08-14 05:37:18 +0000

    new prod

--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -5,6 +5,9 @@ import pic3 from'../assets/fondlaw.png';
 import pic4 from'../assets/fondsport.png';
 import pic5 from'../assets/fondshop.png';
 import pic6 from'../assets/fondspa.png';
+import pic7 from'../assets/fondacce.png';
+import pic8 from'../assets/fonddeco.png';
+
 
 
 
@@ -18,6 +21,11 @@ export default function Works() {
         {id :4 , text : <a href='https://thebeesport.vercel.app/'>Découvrir</a> , image : pic4},
         {id :5 , text : <a href='https://shop-ivoire.vercel.app/'>Découvrir</a> , image : pic5},
         {id :6 , text : <a href='https://spa-online-nine.vercel.app/'>Découvrir</a> , image : pic6},
+    ];
+
+     const groupData3 = [
+        {id :7 , text : <a href='https://ivoire-accessory.vercel.app/'>Découvrir</a> , image : pic7},
+        {id :8 , text : <a href='https://ivoire-deco.vercel.app/'>Découvrir</a> , image : pic8},
     ];
     return(
         <section className='work_main'>
@@ -37,6 +45,16 @@ export default function Works() {
                     <img className='work_img' src={group2.image} alt='couverture'></img>
                     <h4 className='work_title'>
                         {group2.text}
+                    </h4>
+                </div>
+                ))}
+            </div>
+            <div className='work_content'>
+                 {groupData3.map((group3) =>(
+                    <div className='work_box' key={group3.id}>
+                    <img className='work_img' src={group3.image} alt='couverture'></img>
+                    <h4 className='work_title'>
+                        {group3.text}
                     </h4>
                 </div>
                 ))}
